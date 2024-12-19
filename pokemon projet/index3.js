cac2985b52db6d5112790ab3bf996a6efe430bb8
khouloud-izman <izman.khouloud.solicode@gmail.com>
2024-12-19 10:55:40 +0100

    Add files via upload

--- pokemon projet/index3.js
+++ pokemon projet/index3.js
@@ -1,17 +1,13 @@
-// Récupérer l'URL du Pokémon passée en paramètre dans l'URL
 const params = new URLSearchParams(window.location.search);
 const pokemonUrl = params.get("url");
 
 if (pokemonUrl) {
-    // Faire une requête API pour obtenir les détails du Pokémon
     fetch(pokemonUrl)
         .then(response => response.json())
         .then(data => {
-            // Sélectionner le conteneur des détails et vider son contenu
             const detailsContainer = document.querySelector("#pokemon-details");
             detailsContainer.innerHTML = ''; // Effacer les anciens détails
 
-            // Créer des éléments pour afficher le nom, l'image et les statistiques du Pokémon
             const name = document.createElement('h2');
             name.textContent = data.name;
 
@@ -19,16 +15,14 @@ if (pokemonUrl) {
             image.src = data.sprites.front_default;
             image.alt = data.name;
 
-            // Utilisation d'une boucle 'for' classique pour les statistiques
             const stats = document.createElement('ul');
             for (let i = 0; i < data.stats.length; i++) {
                 const stat = data.stats[i];
                 const statItem = document.createElement('li');
-                statItem.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+                statItem.textContent = `${stat.stat.name} : ${stat.base_stat}`;
                 stats.appendChild(statItem);
             }
 
-            // Ajouter les détails à la page
             detailsContainer.appendChild(name);
             detailsContainer.appendChild(image);
             detailsContainer.appendChild(stats);
--- pokemon projet/script.js
+++ pokemon projet/script.js
@@ -1,66 +1,5 @@
-// // Si le conteneur existe, on charge les boutons
-// if (document.querySelector("#container")) {
-//     fetch("https://pokeapi.co/api/v2/type")
-//         .then(response => response.json())
-//         .then(data => {
-//             const container = document.querySelector("#buttons-container");
-//             container.innerHTML = ''; // Efface le contenu existant
-
-//             for (let i = 0; i < data.results.length; i++) {
-//                 const type = data.results[i];
-//                 const button = document.createElement("button");
-//                 button.textContent = type.name;
-//                 button.classList.add("type-button");
-
-//                 button.addEventListener("click", () => {
-//                     window.location.href = `index2.html?name=${type.name}`;
-//                 });
-
-//                 container.appendChild(button);
-//             }
-//         })
-// }
-
-// // Si on est sur index2.html, on affiche les Pokémon par type
-// if (document.querySelector(".page")) {
-//     const name = new URLSearchParams(window.location.search).get("name");
-//     fetch(`https://pokeapi.co/api/v2/type/${name}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             const pokemonDisplay = document.querySelector("#pokemon-display");
-//             pokemonDisplay.innerHTML = ''; // Efface les Pokémon précédents
-
-//             const pokemonList = document.createElement("div");
-//             pokemonList.classList.add("pokemon-list");
-
-//             for (let i = 0; i < data.pokemon.length; i++) {
-//                 const pokemon = data.pokemon[i];
-//                 fetch(pokemon.pokemon.url)
-//                     .then(res => res.json())
-//                     .then(pokemonData => {
-//                         const pokemonItem = document.createElement("div");
-//                         pokemonItem.classList.add("pokemon-item");
-
-//                         const pokemonName = document.createElement("p");
-//                         pokemonName.textContent = pokemon.pokemon.name;
-
-//                         const pokemonImage = document.createElement("img");
-//                         pokemonImage.src = pokemonData.sprites.front_default;
-
-//                         pokemonItem.appendChild(pokemonImage);
-//                         pokemonItem.appendChild(pokemonName);
-//                         pokemonList.appendChild(pokemonItem);
-//                     });
-//             }
-
-//             pokemonDisplay.appendChild(pokemonList);
-//         })
-// }
 
 
-
-
-// Si le conteneur existe, on charge les boutons
 if (document.querySelector("#container")) {
     fetch("https://pokeapi.co/api/v2/type")
         .then(response => response.json())
@@ -83,7 +22,6 @@ if (document.querySelector("#container")) {
         })
 }
 
-// Si on est sur index2.html, on affiche les Pokémon par type
 if (document.querySelector(".page")) {
     const name = new URLSearchParams(window.location.search).get("name");
     fetch(`https://pokeapi.co/api/v2/type/${name}`)
@@ -109,7 +47,6 @@ if (document.querySelector(".page")) {
                         const pokemonImage = document.createElement("img");
                         pokemonImage.src = pokemonData.sprites.front_default;
 
-                        // Ajouter l'événement de clic pour afficher les détails du Pokémon
                         pokemonItem.addEventListener("click", () => {
                             window.location.href = `index3.html?url=${pokemon.pokemon.url}`;
                         });
